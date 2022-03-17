75bed6b0ec5791fa0f8d14cb0fc32303fab84d70
Harris Spivack <harris.spivack@gmail.com>
2022-03-17 10:26:02 -0500

    using System.CommandLine to build sample app as cli

--- BlockCypher.Client.SampleConsole/Program.cs
+++ BlockCypher.Client.SampleConsole/Program.cs
@@ -1,14 +1,25 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+using BlockCypher.Clients;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using NBitcoin;
 using Serilog;
 
 namespace BlockCypher.Client.SampleConsole;
 
 public class Program
 {
+    public class Commands
+    {
+        public const string BLOCKCHAIN = "blockchain";
+        public const string ADDRESS = "address";
+    }
     public static async Task<int> Main(string[] args)
     {
+        var host = Host.CreateDefaultBuilder(args);
         try
         {
             Log.Logger = new LoggerConfiguration()
@@ -21,12 +32,42 @@ public class Program
 
             PrintWelcomeText();
 
-            return await Host.CreateDefaultBuilder(args)
-                .ConfigureServices(ConfigureServices)
-                .UseSerilog()
-                .Build()
-                .Services.GetRequiredService<IConsoleApplication>()
-                .ExecuteAsync(args);
+            var blockChainCommand = new Command(Commands.BLOCKCHAIN, "Examples of calling blockchain api")
+            {
+                Handler = CommandHandler.Create<CoinChain?, bool>((coinChain, showRestLogs) =>
+                {
+                    Log.Logger.Debug("Running {command} on {coinChain}", Commands.BLOCKCHAIN, coinChain);
+                    return BuildHost<SampleConsoleApplication>(host, coinChain, showRestLogs)
+                        .Services
+                        .GetRequiredService<IConsoleApplication>()
+                        .ExecuteAsync(args);
+                })
+            };
+
+            var addressChainCommand = new Command(Commands.ADDRESS, "Examples of calling address api")
+            {
+                Handler = CommandHandler.Create<CoinChain?, bool>((coinChain, showRestLogs) =>
+                {
+                    Log.Logger.Debug("Running {command} on {coinChain}", Commands.ADDRESS, coinChain);
+                    return Console.Out.WriteLineAsync($"I will do address things! {coinChain} {showRestLogs}");
+                })
+            };
+
+            var rootCommand = new RootCommand
+            {
+                blockChainCommand,
+                addressChainCommand
+            };
+
+            rootCommand.AddGlobalOption(new Option<CoinChain?>("--coin-chain",
+                () => null,
+                "Coin and Chain to use.  If not set value from appsettings.config will be used.  If that's not set, then BitcoinMain will be used"));
+
+            rootCommand.AddGlobalOption(new Option<bool>("--show-rest-logs",
+                () => false,
+                "If present, REST logs will be printed to console.  They will always be logged to serilog!"));
+
+            return await rootCommand.InvokeAsync(args);
         }
         catch (Exception ex)
         {
@@ -39,28 +80,48 @@ public class Program
         }
     }
 
-    private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+    /// <summary>
+    /// Builds the host after the cli commands and options have been parsed
+    /// </summary>
+    private static IHost BuildHost<TCommandRunner>(IHostBuilder host, CoinChain? coinChain, bool showRestLogs)
+        where TCommandRunner : class, IConsoleApplication
     {
-        services.AddSingleton<IConsoleApplication, SampleConsoleApplication>();
+        return host
+            .ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<IConsoleApplication, TCommandRunner>();
 
-        services.AddTransient<ConsoleLoggingDelegatingHandler>();
-        services.AddBlockCypherClient(options =>
-                context.Configuration.GetSection("BlockCypherClientOptions").Bind(options))
-            .AddDefaultRetryPolicy()
-            .AddHttpMessageHandler<ConsoleLoggingDelegatingHandler>();
+                var httpBuilder = services.AddBlockCypherClient(options =>
+                    {
+                        context.Configuration.GetSection("BlockCypherClientOptions").Bind(options);
+                        if (coinChain.HasValue)
+                        {
+                            options.DefaultCoinChain = coinChain.Value;
+                        }
+                    })
+                    .AddDefaultRetryPolicy();
+
+                if (showRestLogs)
+                {
+                    services.AddTransient<ConsoleLoggingDelegatingHandler>();
+                    httpBuilder.AddHttpMessageHandler<ConsoleLoggingDelegatingHandler>();
+                }
+            })
+            .UseSerilog()
+            .Build();
     }
 
     private static void PrintWelcomeText()
     {
-        Console.WriteLine(@"Thank you for running the BlockCypher.Client sample console application!
-╔══╗╔╗──────╔╗─╔═══╗──────╔╗──────╔═══╦╗───────╔╗
-║╔╗║║║──────║║─║╔═╗║──────║║──────║╔═╗║║──────╔╝╚╗
-║╚╝╚╣║╔══╦══╣║╔╣║─╚╬╗─╔╦══╣╚═╦══╦═╣║─╚╣║╔╦══╦═╬╗╔╝
-║╔═╗║║║╔╗║╔═╣╚╝╣║─╔╣║─║║╔╗║╔╗║║═╣╔╣║─╔╣║╠╣║═╣╔╗╣║
-║╚═╝║╚╣╚╝║╚═╣╔╗╣╚═╝║╚═╝║╚╝║║║║║═╣╠╣╚═╝║╚╣║║═╣║║║╚╗
-╚═══╩═╩══╩══╩╝╚╩═══╩═╗╔╣╔═╩╝╚╩══╩╩╩═══╩═╩╩══╩╝╚╩═╝
-───────────────────╔═╝║║║
-───────────────────╚══╝╚╝");
+        Console.WriteLine(@"Thank you for running the BlockCypher.Client Sample cli!
+─╔══╗╔╗──────╔╗─╔═══╗──────╔╗──────╔═══╦╗───────╔╗──
+─║╔╗║║║──────║║─║╔═╗║──────║║──────║╔═╗║║──────╔╝╚╗─
+─║╚╝╚╣║╔══╦══╣║╔╣║─╚╬╗─╔╦══╣╚═╦══╦═╣║─╚╣║╔╦══╦═╬╗╔╝─
+─║╔═╗║║║╔╗║╔═╣╚╝╣║─╔╣║─║║╔╗║╔╗║║═╣╔╣║─╔╣║╠╣║═╣╔╗╣║──
+─║╚═╝║╚╣╚╝║╚═╣╔╗╣╚═╝║╚═╝║╚╝║║║║║═╣╠╣╚═╝║╚╣║║═╣║║║╚╗─
+─╚═══╩═╩══╩══╩╝╚╩═══╩═╗╔╣╔═╩╝╚╩══╩╩╩═══╩═╩╩══╩╝╚╩═╝─
+────────────────────╔═╝║║║──────────────────────────
+────────────────────╚══╝╚╝──────────────────────────");
         Console.WriteLine();
         Console.WriteLine();
     }
